7ccc0876bf4662b88e4b929df5d5c593f9ee4a0f
Stable Consulting Engineers <admin@stableconsulting.co.nz>
2025-05-29 14:46:35 +1000

    feat: add App component and styles for React application

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to SCEG Details</h1>
+        <p>Your application is now running!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App; 
